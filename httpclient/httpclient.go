9871a3b4e77e959c259984c0117b62388cbe7947
alex <lisuo@coinmex.com>
2019-05-23 13:36:04 +0800

    * 增加http.Transport.MaxIdleConnsPeerHost

--- httpclient/httpclient.go
+++ httpclient/httpclient.go
@@ -15,17 +15,20 @@ package httpclient
 
 import (
     "log"
+    "net"
     "net/http"
     "io/ioutil"
     "bytes"
     "strings"
     "fmt"
+    "time"
 )
 
 type HttpClient struct {
     BaseUrl string
     Headers map[string]string
     PrintLogs bool
+    client *http.Client
 }
 
 func NewHttpClient(baseUrl string, headers map[string]string, printLogs bool) *HttpClient {
@@ -33,6 +36,21 @@ func NewHttpClient(baseUrl string, headers map[string]string, printLogs bool) *H
     httpClient.BaseUrl = baseUrl
     httpClient.Headers = headers
     httpClient.PrintLogs = printLogs
+    httpClient.client = &http.Client{
+        Transport:&http.Transport{
+            Proxy: http.ProxyFromEnvironment,
+            DialContext: (&net.Dialer{
+                Timeout:   30 * time.Second,
+                KeepAlive: 30 * time.Second,
+                DualStack: true,
+            }).DialContext,
+            MaxIdleConns:          100,
+            IdleConnTimeout:       90 * time.Second,
+            TLSHandshakeTimeout:   10 * time.Second,
+            ExpectContinueTimeout: 1 * time.Second,
+            MaxIdleConnsPerHost: 100,
+        },
+    }
     return httpClient
 }
 
@@ -92,8 +110,8 @@ func (c *HttpClient) httpRequest(url string, requestType string, headers map[str
         c.logSendRequest(url, requestType, body)
     }
 
-    client := &http.Client{}
-    resp, err := client.Do(req)
+    //client := &http.Client{}
+    resp, err := c.client.Do(req)
     if err != nil {
         return "", err
     }
